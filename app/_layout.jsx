e48fffc4f2d0045278e0e0dc564d1c68b9411e78
Ohani Kizito <kizitoohani@gmail.com>
2025-01-15 16:52:35 +0100

    commit

---
+++ app/_layout.jsx
@@ -0,0 +1,10 @@
+import "../global.css"
+import React from 'react'
+import { Slot } from 'expo-router'
+import { View, Text } from 'react-native'
+
+const _layout = () => {
+    return <Slot />
+}
+
+export default _layout
---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Welcome = () => {
+    return (
+        <View className="flex items-center justify-center w-full h-screen">
+            <Text className="text-3xl text-red-600 ">_layout</Text>
+        </View>
+    )
+}
+
+export default Welcome
