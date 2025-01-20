8f7029d643a9af8be5701cf83fa4cc94d0fa7e94
Ohani Kizito <kizitoohani@gmail.com>
2025-01-20 14:56:32 +0100

    added the carousel data

---
+++ constants/carouselData.js
@@ -0,0 +1,34 @@
+import { images } from './images';
+
+export default [
+    {
+        id: 1,
+        img: images.carousel1,
+        title: 'Control Your Finances with Personal Budgets',
+        description: 'Take charge of your financial life with personalized budgets designed to help you save and spend wisely.',
+    },
+    {
+        id: 2,
+        img: images.carousel2,
+        title: 'AI Finance Assistant, Everywhere, Anywhere.',
+        description: 'Your smart companion for managing money on the go, making every transaction smarter.',
+    },
+    {
+        id: 3,
+        img: images.carousel3,
+        title: 'Set Your Own Financial Goals Easily',
+        description: 'Define your savings goals and track your progress effortlessly with intuitive tools.',
+    },
+    {
+        id: 4,
+        img: images.carousel4,
+        title: 'Save More & Spend Smarter',
+        description: 'Optimize your spending habits and grow your savings with actionable insights.',
+    },
+    {
+        id: 5,
+        img: images.carousel5,
+        title: 'Manage Subscriptions in One Single Place',
+        description: 'Keep all your subscriptions organized and avoid surprises with a centralized management tool.',
+    },
+];
--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,15 @@
 import logo from "../assets/images/logo.png";
+import carousel1 from "../assets/images/carousel_l.png";
+import carousel2 from "../assets/images/carousel_2.png";
+import carousel3 from "../assets/images/carousel_3.png";
+import carousel4 from "../assets/images/carousel_4.png";
+import carousel5 from "../assets/images/carousel_5.png";
 
 export default {
     logo,
+    carousel1,
+    carousel2,
+    carousel3,
+    carousel4,
+    carousel5,
 };
