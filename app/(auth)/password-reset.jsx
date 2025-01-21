b2cc4d7dec49d74b0119526b0bd8c6e33988329d
Ohani Kizito <kizitoohani@gmail.com>
2025-01-21 13:52:46 +0100

    Update password-reset.jsx

--- app/(auth)/password-reset.jsx
+++ app/(auth)/password-reset.jsx
@@ -1,12 +1,55 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, Dimensions, Image } from "react-native";
+
+import { images } from "../../constants";
+import { CustomButton } from "../../components";
 
 const PasswordReset = () => {
     return (
-        <View>
-            <Text>PasswordReset</Text>
-        </View>
-    )
-}
+        <SafeAreaView className="bg-white h-full">
+            <ScrollView>
+                <View
+                    className="w-full flex justify-center items-center h-full px-4 mb-6"
+                    style={{
+                        minHeight: Dimensions.get("window").height - 100,
+                    }}
+                >
+                    <Image
+                        source={images.auth}
+                        resizeMode="contain"
+                        className="w-96 h-60"
+                    />
+
+                    <Text className="text-4xl text-gray-800 mt-2 font-dsblack text-center">
+                        Password Reset Sent.
+                    </Text>
+                    <Text className="text-xl text-gray-600 mt-2 font-dsregular text-center">
+                        Please check your email in a few minutes - we’ve sent you an email containing password recovery link.
+                    </Text>
+
+                    <CustomButton
+                        primary
+                        title="Continue"
+                        handlePress={() => {
+                            router.push("/sign-in")
+                        }}
+                        containerStyles="mt-8"
+                    />
+
+                    <View className="max-w-[240px] mt-5">
+                        <Text className="text-base text-gray-600 font-dsregular mt-4">Don’t remember your email?
+                            {""} Contact us at {""}
+                            <Link
+                                className="text-primary font-dsbold underline"
+                                href="mailto:help@finpal.ai"
+                            >help@finpal.ai</Link>
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
 
 export default PasswordReset
